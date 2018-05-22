dfcf331c971b671a3e0a8977c6f1017cc1e55ac8
wangsheng <752958369@qq.com>
2018-05-22 16:51:07 +0800

    add android platform

---
+++ android/src/main/java/com/unionpay/UnionPayModule.java
@@ -0,0 +1,74 @@
+package com.unionpay;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+/**
+ * Created by USER on 2018/5/17.
+ */
+
+public class UnionPayModule extends ReactContextBaseJavaModule implements ActivityEventListener {
+
+    public UnionPayModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        reactContext.addActivityEventListener(this);
+    }
+
+    @Override
+    public String getName() {
+        return "UPPayControl";
+    }
+
+    private Promise mPromise;
+    @ReactMethod
+    public void pay(String tn,boolean isProduction,Promise promise){
+        String mode = "01";
+        if(isProduction){
+            mode = "00";
+        }
+        mPromise = promise;
+        UPPayAssistEx.startPay(getCurrentActivity(), null, null, tn, mode);
+    }
+
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        /*************************************************
+         * 步骤3：处理银联手机支付控件返回的支付结果
+         ************************************************/
+        if (data == null || mPromise == null) {
+            return;
+        }
+        String msg = "";
+        /*
+         * 支付控件返回字符串:success、fail、cancel 分别代表支付成功，支付失败，支付取消
+         */
+        String str = data.getExtras().getString("pay_result");
+        if (str.equalsIgnoreCase("success")) {
+            // 如果想对结果数据验签，可使用下面这段代码，但建议不验签，直接去商户后台查询交易结果
+            // result_data结构见c）result_data参数说明
+            if (data.hasExtra("result_data")) {
+                String result = data.getExtras().getString("result_data");
+                mPromise.resolve(result);
+            }
+            // 结果result_data为成功时，去商户后台查询一下再展示成功
+        } else if (str.equalsIgnoreCase("fail")) {
+            mPromise.reject("0", "fail");
+        } else if (str.equalsIgnoreCase("cancel")) {
+            mPromise.reject("0", "cancel");
+        }
+    }
+
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        if(intent != null){
+            String str = intent.getExtras().getString("pay_result");
+        }
+    }
+}
---
+++ android/src/main/java/com/unionpay/UnionPayPackage.java
@@ -0,0 +1,34 @@
+package com.unionpay;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by USER on 2018/5/17.
+ */
+
+public class UnionPayPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new UnionPayModule(reactContext));
+        return modules;
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
